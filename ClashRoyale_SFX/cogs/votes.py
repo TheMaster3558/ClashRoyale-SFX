021f71f5503e13c3af03568664ef6da5f0f99faa
chawkk6404 <chawk_k@outlook.com>
2024-04-10 10:31:53 -0700

    Remove discordbotlist

--- ClashRoyale_SFX/cogs/votes.py
+++ ClashRoyale_SFX/cogs/votes.py
@@ -1,16 +1,12 @@
 from __future__ import annotations
 
-import asyncio
 import math
 import os
-import ssl
 import random
 from typing import TYPE_CHECKING
 
-import aiohttp_cors
 import discord
 import topgg
-from aiohttp import web
 from discord import app_commands
 from discord.ext import commands
 
@@ -23,25 +19,16 @@ class NoMoreCommands(app_commands.CheckFailure):
 
 
 class TopGGVoteButton(discord.ui.Button):
-    def __init__(self, bot_url_name: str, bot_id: int) -> None:
+    def __init__(self, bot_id: int) -> None:
         super().__init__(
             label='Vote on Top.gg', url=f'https://top.gg/bot/{bot_id}/vote', emoji='<:topgg_logo:1189808860807049298>'
         )
 
 
 class TopGGReviewButton(discord.ui.Button):
-    def __init__(self, bot_url_name: str, bot_id: int) -> None:
+    def __init__(self, bot_id: int) -> None:
         super().__init__(
             label='Review on Top.gg', url=f'https://top.gg/bot/{bot_id}#reviews', emoji='<:topgg_logo:1189808860807049298>'
-        )
-
-
-class DiscordBotListVoteButton(discord.ui.Button):
-    def __init__(self, bot_url_name: str, bot_id: int) -> None:
-        super().__init__(
-            label='Vote on discordbotlist.com',
-            url=f'https://discordbotlist.com/bots/{bot_url_name}/upvote',
-            emoji='<:discordbotlist_logo:1189810709236830208>',
         )
 
 
@@ -52,11 +39,11 @@ class BaseButtonsView(discord.ui.View):
         super().__init__()
 
         for button_cls in self.buttons:
-            self.add_item(button_cls('clashroyale-sfx', bot.application_id))  # type: ignore
+            self.add_item(button_cls(bot.application_id))  # type: ignore
 
 
 class VoteView(BaseButtonsView):
-    buttons = (TopGGVoteButton, DiscordBotListVoteButton)
+    buttons = (TopGGVoteButton,)
 
 
 class ReviewView(BaseButtonsView):
@@ -64,7 +51,7 @@ class ReviewView(BaseButtonsView):
 
 
 class AllView(BaseButtonsView):
-    buttons = (TopGGVoteButton, TopGGReviewButton, DiscordBotListVoteButton)
+    buttons = (TopGGVoteButton, TopGGReviewButton)
 
 
 class TopGG(commands.Cog):
@@ -75,44 +62,12 @@ class TopGG(commands.Cog):
         self.port = int(os.environ['PORT'])
 
         self.topgg_webhook = topgg.WebhookManager(bot).dbl_webhook('/topgg', self.webhook_auth)
-        cors = aiohttp_cors.setup(self.topgg_webhook.webserver, defaults={
-            '*': aiohttp_cors.ResourceOptions(
-                allow_credentials=True,
-                expose_headers='*',
-                allow_headers='*'
-            )
-        })
-        cors.add(self.topgg_webhook.webserver.router.add_post('/discordbotlist', self.discordbotlist_webhook))
-
-    async def run(self) -> None:
-        for webhook in self.topgg_webhook._webhooks.values():
-            self.topgg_webhook.webserver.router.add_post(webhook['route'], webhook['func'])
-        runner = web.AppRunner(self.topgg_webhook.webserver)
-        await runner.setup()
-
-        ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
-        self.topgg_webhook._webserver = web.TCPSite(
-            runner,
-            host='0.0.0.0',
-            port=self.port,
-            ssl_context=ssl_context
-        )
-        await self.topgg_webhook._webserver.start()
 
     async def cog_load(self) -> None:
-        asyncio.create_task(self.run())
+        self.topgg_webhook.run(self.port)
 
     async def cog_unload(self) -> None:
         await self.topgg_webhook.close()
-
-    async def discordbotlist_webhook(self, request: web.Request) -> web.Response:
-        if request.headers.get('Authorization', '') != self.webhook_auth:
-            return web.Response(status=401, text='Unauthorized')
-
-        data = await request.json()
-
-        self.bot.dispatch('vote', int(data['id']))
-        return web.Response(status=200, text='Ok')
 
     @commands.Cog.listener()
     async def on_dbl_vote(self, data: topgg.types.BotVoteData) -> None:
